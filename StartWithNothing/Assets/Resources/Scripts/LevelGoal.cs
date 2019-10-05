fc717bb1f9191a1ea6a06c36bc51deaba2495a8b
Warren Atchison <35613494+WarrenAtchison@users.noreply.github.com>
2019-10-05 15:32:21 -0400

    added level 3
    moving platform introduction level!

--- StartWithNothing/Assets/Resources/Scripts/LevelGoal.cs
+++ StartWithNothing/Assets/Resources/Scripts/LevelGoal.cs
@@ -6,11 +6,14 @@ public class LevelGoal : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneHandler sceneHandler = GameObject.Find("SceneHandler").GetComponent<SceneHandler>();
-        int newScene = sceneHandler.levels.IndexOf("Level1") + 1;
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            SceneHandler sceneHandler = GameObject.Find("SceneHandler").GetComponent<SceneHandler>();
+            int newScene = sceneHandler.levels.IndexOf("Level1") + 1;
 
-        Debug.Log("attempting to load " + sceneHandler.levels[newScene]);
+            Debug.Log("attempting to load " + sceneHandler.levels[newScene]);
 
-        sceneHandler.ChangeScenes(sceneHandler.levels[newScene]);
+            sceneHandler.ChangeScenes(sceneHandler.levels[newScene]);
+        }
     }
 }
