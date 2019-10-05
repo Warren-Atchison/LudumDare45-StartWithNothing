76505c69be3525a311943e2635ce1d18a08397bf
Miguel B <40647244+Migillope@users.noreply.github.com>
2019-10-05 03:30:33 -0400

    Added spikes
    Added spikes prefab to Hazards

---
+++ StartWithNothing/Assets/Resources/Scripts/Spikes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    private GameObject player;
+    void Start(){
+        player = GameObject.Find("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        Debug.Log("ur ded");
+    }
+}
