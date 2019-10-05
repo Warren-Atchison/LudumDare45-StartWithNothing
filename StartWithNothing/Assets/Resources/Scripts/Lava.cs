3e1fb9c76255756cf8321a95b18555c813be5f30
Warren Atchison <35613494+WarrenAtchison@users.noreply.github.com>
2019-10-05 18:08:57 -0400

    Added lava and wraparound
    lava for preventing wraparound on bottom of screen for now.

---
+++ StartWithNothing/Assets/Resources/Scripts/Lava.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+            collision.gameObject.GetComponent<PlayerController>().Die();
+        else
+            Destroy(collision.gameObject);
+    }
+}
