b788a167121ad17eaf2ceb7c2e10bd9dac58ca1b
Garrett Fite <36345799+Stranded-Helix@users.noreply.github.com>
2019-10-05 14:34:03 -0500

    Partial Cannonball and Cannon logic

---
+++ StartWithNothing/Assets/CannonLauncher.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonLauncher : MonoBehaviour
+{
+    public GameObject cannonball;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("FireCannon");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("t"))
+        {
+            CreateCannonball();
+        }
+    }
+
+    IEnumerable FireCannon()
+    {
+        while (true)
+        {
+            CreateCannonball();
+            yield return new WaitForSeconds(4);
+        }
+    }
+    void CreateCannonball()
+    {
+        Instantiate(cannonball);
+    }
+}
