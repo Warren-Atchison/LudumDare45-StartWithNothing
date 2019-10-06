632fe0f9cf08e1df0f9d5f55df2a9d3df80123a1
Warren Atchison <35613494+WarrenAtchison@users.noreply.github.com>
2019-10-06 18:46:33 -0400

    changed the level template and fixed class punctuation with cannonballHandler
    as summary

--- StartWithNothing/Assets/Resources/Scripts/cannonballHandler.cs
+++ StartWithNothing/Assets/Resources/Scripts/cannonballHandler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cannonballHandler : MonoBehaviour
+public class CannonballHandler : MonoBehaviour
 {
     //public float ShotSpeedX;
     //public float ShotSpeedY;
