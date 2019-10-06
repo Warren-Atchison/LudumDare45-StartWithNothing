5ffcc5c4354979ec26d403b72450bccd7fb25798
Miguel B <40647244+Migillope@users.noreply.github.com>
2019-10-06 04:46:21 -0400

    Added scripts for Fast and Slow Zones
    No prefab, I don't know if we want it to be a zone (like water) or a platform type (like falling platform).

---
+++ StartWithNothing/Assets/Resources/Scripts/SlowZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowZone : MonoBehaviour
+{
+    private GameObject player;
+    float defaultMoveSpeed;
+    private PlayerController pc;
+    private Rigidbody2D rb;
+
+    void Start(){
+        player = GameObject.Find("Player");
+        pc = player.GetComponent<PlayerController>();
+        rb = player.GetComponent<Rigidbody2D>();
+
+        defaultMoveSpeed = pc.moveSpeed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision){
+        pc.moveSpeed = defaultMoveSpeed/2;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){
+        pc.moveSpeed = defaultMoveSpeed;
+    }
+}
