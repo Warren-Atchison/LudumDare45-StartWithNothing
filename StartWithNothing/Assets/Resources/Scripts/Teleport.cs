0fc79c443599c58a738a2ff09f3ea1ee6ee12690
julia-berger <35280099+julia-berger@users.noreply.github.com>
2019-10-06 18:04:46 -0400

    Teleport prefab created

---
+++ StartWithNothing/Assets/Resources/Scripts/Teleport.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    public GameObject teleportDestination;
+
+    public enum Direction
+    {
+        LEFT, RIGHT, UP, DOWN
+    }
+
+    public Direction exitDirection = Direction.RIGHT;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject objectToTeleport = collision.gameObject;
+
+        float widthObjectToTeleport = collision.bounds.size.x;
+        float widthTeleportDestination = teleportDestination.GetComponent<BoxCollider2D>().bounds.size.x;
+
+        float heightObjectToTeleport = collision.bounds.size.y;
+        float heightTeleportDestination = teleportDestination.GetComponent<BoxCollider2D>().bounds.size.y;
+
+        float targetX, targetY;
+        Direction targetDirection = teleportDestination.GetComponent<Teleport>().exitDirection;
+
+        if ( targetDirection == Direction.RIGHT || targetDirection == Direction.LEFT)
+        {
+            int xShift = (targetDirection == Direction.LEFT) ? -1 : 1;
+            targetX = teleportDestination.transform.position.x + xShift * ((widthObjectToTeleport / 2) + (widthTeleportDestination / 2) + 0.03f);
+            targetY = teleportDestination.transform.position.y - (teleportDestination.GetComponent<BoxCollider2D>().bounds.size.y / 2) + (collision.bounds.size.y / 2);
+        }   
+        else
+        {
+            int yShift = (targetDirection == Direction.DOWN) ? -1 : 1;
+            targetX = teleportDestination.transform.position.x;
+            targetY = teleportDestination.transform.position.y + yShift * ((heightObjectToTeleport / 2) + (heightTeleportDestination / 2) + 0.03f);
+        }
+
+        Vector2 targetPos = new Vector2(targetX, targetY);
+        objectToTeleport.transform.position = targetPos;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
