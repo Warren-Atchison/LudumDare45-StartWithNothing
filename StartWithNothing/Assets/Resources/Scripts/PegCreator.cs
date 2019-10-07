1ec23c0fe3844a2ef42798a7cabc9920c6d4a8b4
Garrett Fite <36345799+Stranded-Helix@users.noreply.github.com>
2019-10-06 23:51:35 -0500

    Added Level 10 with scripts
    Added Level 10 with peg prefab and pegcreator

---
+++ StartWithNothing/Assets/Resources/Scripts/PegCreator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PegCreator : MonoBehaviour
+{
+    public GameObject peg;
+
+    void Awake()
+    {
+        StartCoroutine("CreatePegs");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        peg = GetComponent<GameObject>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("t"))
+        {
+            StartCoroutine("CreatePegs");
+        }
+    }
+
+    IEnumerator CreatePegs()
+    {
+        Instantiate(peg, new Vector2(-8.5f, -1f), Quaternion.identity);
+        Instantiate(peg, new Vector2(-8.5f, 1f), Quaternion.identity);
+        for (float x = -8; x <= 8; x += 1f)
+        {
+            for (float y = -2; y <=2; y += 2f)
+            {
+                Instantiate(peg, new Vector2(x, y), Quaternion.identity);
+                 
+            }
+
+            for (float z = -1; z <=1; z += 2f)
+            {
+                Instantiate(peg, new Vector2(x+0.5f, z), Quaternion.identity);
+            }
+            
+        }
+        yield return null;
+    }
+}
