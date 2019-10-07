f7bac77c1e22b9825a789bd304a28aec35b9416e
Miguel B <40647244+Migillope@users.noreply.github.com>
2019-10-06 23:40:09 -0400

    Created Level 13; Switched the names of 7 & 11
    Level 13 is an underwater level with stuff

--- StartWithNothing/Assets/Resources/Scripts/cannonballHandler.cs
+++ StartWithNothing/Assets/Resources/Scripts/cannonballHandler.cs
@@ -8,19 +8,14 @@ public class cannonballHandler : MonoBehaviour
     //public float ShotSpeedY;
     private Rigidbody2D rb;
     private GameObject player;
+    
     // Start is called before the first frame update
     void Start()
     {
         //rb = GetComponent<Rigidbody2D>();
         //rb.AddForce(new Vector2(ShotSpeedX,ShotSpeedY));
-        Destroy(gameObject, 6.0f);
+        Destroy(this, 6.0f);
         player = GameObject.Find("Player");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
